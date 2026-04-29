3a204eaba3ae45df483f276abf91c0208bedb52b
henjue <henjue@gmail.com>
2026-04-30 00:19:34 +0800

    update

--- ui/screens/home_gen.c
+++ ui/screens/home_gen.c
@@ -233,6 +233,7 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_9 = lv_label_create(lv_obj_8);
     lv_label_set_text(lv_label_9, "750");
     lv_obj_set_style_text_color(lv_label_9, lv_color_hex(0xffd208), 0);
+    lv_obj_set_style_text_font(lv_label_9, hos_14, 0);
     
     lv_obj_t * lv_obj_9 = lv_obj_create(lv_obj_7);
     lv_obj_set_width(lv_obj_9, lv_pct(73));
@@ -259,6 +260,7 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_10 = lv_label_create(lv_obj_9);
     lv_label_set_text(lv_label_10, "0%");
     lv_obj_set_style_text_color(lv_label_10, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_10, hos_14, 0);
     
     lv_obj_t * lv_obj_10 = lv_obj_create(lv_obj_0);
     lv_obj_set_style_flex_grow(lv_obj_10, 1, 0);
@@ -304,6 +306,7 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_11 = lv_label_create(lv_obj_12);
     lv_label_set_text(lv_label_11, "CH1");
     lv_obj_set_style_text_color(lv_label_11, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_11, hos_regular, 0);
     
     lv_obj_t * lv_obj_13 = lv_obj_create(lv_obj_11);
     lv_obj_set_style_flex_grow(lv_obj_13, 1, 0);
@@ -321,14 +324,17 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_12 = lv_label_create(lv_obj_13);
     lv_label_set_text(lv_label_12, "0.000");
     lv_obj_set_style_text_color(lv_label_12, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_12, hos_14, 0);
     
     lv_obj_t * lv_label_13 = lv_label_create(lv_obj_13);
     lv_label_set_text(lv_label_13, "0.000");
     lv_obj_set_style_text_color(lv_label_13, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_13, hos_14, 0);
     
     lv_obj_t * lv_label_14 = lv_label_create(lv_obj_13);
     lv_label_set_text(lv_label_14, "0.000");
     lv_obj_set_style_text_color(lv_label_14, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_14, hos_14, 0);
     
     lv_obj_t * lv_obj_14 = lv_obj_create(lv_obj_10);
     lv_obj_set_width(lv_obj_14, lv_pct(32));
@@ -359,6 +365,7 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_15 = lv_label_create(lv_obj_15);
     lv_label_set_text(lv_label_15, "CH2");
     lv_obj_set_style_text_color(lv_label_15, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_15, hos_regular, 0);
     
     lv_obj_t * lv_obj_16 = lv_obj_create(lv_obj_14);
     lv_obj_set_style_flex_grow(lv_obj_16, 1, 0);
@@ -376,14 +383,17 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_16 = lv_label_create(lv_obj_16);
     lv_label_set_text(lv_label_16, "0.000");
     lv_obj_set_style_text_color(lv_label_16, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_16, hos_14, 0);
     
     lv_obj_t * lv_label_17 = lv_label_create(lv_obj_16);
     lv_label_set_text(lv_label_17, "0.000");
     lv_obj_set_style_text_color(lv_label_17, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_17, hos_14, 0);
     
     lv_obj_t * lv_label_18 = lv_label_create(lv_obj_16);
     lv_label_set_text(lv_label_18, "0.000");
     lv_obj_set_style_text_color(lv_label_18, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_18, hos_14, 0);
     
     lv_obj_t * lv_obj_17 = lv_obj_create(lv_obj_10);
     lv_obj_set_width(lv_obj_17, lv_pct(32));
@@ -414,6 +424,7 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_19 = lv_label_create(lv_obj_18);
     lv_label_set_text(lv_label_19, "CH3");
     lv_obj_set_style_text_color(lv_label_19, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_19, hos_regular, 0);
     
     lv_obj_t * lv_obj_19 = lv_obj_create(lv_obj_17);
     lv_obj_set_style_flex_grow(lv_obj_19, 1, 0);
@@ -431,14 +442,17 @@ lv_obj_t * home_create(void)
     lv_obj_t * lv_label_20 = lv_label_create(lv_obj_19);
     lv_label_set_text(lv_label_20, "0.000");
     lv_obj_set_style_text_color(lv_label_20, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_20, hos_14, 0);
     
     lv_obj_t * lv_label_21 = lv_label_create(lv_obj_19);
     lv_label_set_text(lv_label_21, "0.000");
     lv_obj_set_style_text_color(lv_label_21, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_21, hos_14, 0);
     
     lv_obj_t * lv_label_22 = lv_label_create(lv_obj_19);
     lv_label_set_text(lv_label_22, "0.000");
     lv_obj_set_style_text_color(lv_label_22, lv_color_hex(0xFFFFFF), 0);
+    lv_obj_set_style_text_font(lv_label_22, hos_14, 0);
 
     LV_TRACE_OBJ_CREATE("finished");
 
