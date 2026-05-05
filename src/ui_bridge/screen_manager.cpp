d8d1e89a0e0939117459f3f089a69c43b49f8b90
henjue <henjue@gmail.com>
2026-05-05 18:40:39 +0800

    debug(splash): add detailed logging to diagnose splash hang
    Added Serial.println throughout splash init and animation flow
    to identify where execution stops.

--- src/ui_bridge/screen_manager.cpp
+++ src/ui_bridge/screen_manager.cpp
@@ -2,6 +2,7 @@
 #include "splash_anim.h"
 #include "settings_ui.h"
 #include "lvgl/lvgl.h"
+#include <Arduino.h>
 
 extern "C" lv_obj_t* splash_create(void);
 extern "C" lv_obj_t* home_create(void);
@@ -12,6 +13,7 @@ namespace {
     uint32_t g_splash_duration_ms = 1500;
 
     void splash_timer_cb(lv_timer_t* t) {
+        Serial.println("[Splash] Timer fired, loading home screen");
         lv_screen_load(g_home);
         if (g_splash) {
             lv_obj_delete(g_splash);
@@ -21,25 +23,38 @@ namespace {
     }
 
     void async_splash_init(void*) {
-        if (g_splash == nullptr) return;
+        Serial.println("[Splash] async_splash_init called");
+        if (g_splash == nullptr) {
+            Serial.println("[Splash] ERROR: g_splash is null");
+            return;
+        }
+        Serial.println("[Splash] Calling splash_play_intro");
         ui_bridge::splash_play_intro(g_splash);
+        Serial.println("[Splash] Creating splash timer");
         lv_timer_t* t = lv_timer_create(splash_timer_cb, g_splash_duration_ms, nullptr);
         lv_timer_set_repeat_count(t, 1);
+        Serial.println("[Splash] Timer created");
     }
 }
 
 namespace ui_bridge {
     void screen_manager_init(uint32_t splash_duration_ms) {
+        Serial.println("[Splash] screen_manager_init start");
         g_home = home_create();
+        Serial.println("[Splash] home_create done");
 
         g_splash = splash_create();
+        Serial.println("[Splash] splash_create done");
         lv_screen_load(g_splash);
+        Serial.println("[Splash] splash screen loaded");
         g_splash_duration_ms = splash_duration_ms;
 
-        // 延迟播放动画，等待 LVGL 任务启动
+        Serial.println("[Splash] Calling lv_async_call");
         lv_async_call(async_splash_init, nullptr);
+        Serial.println("[Splash] lv_async_call returned");
 
         settings_ui::init();
+        Serial.println("[Splash] screen_manager_init done");
     }
 
     lv_obj_t* screen_manager_get_home() {
